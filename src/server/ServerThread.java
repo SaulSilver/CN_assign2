a51039c028a0b20be82dbc086f2877b1c6744a5e
SaulSilver <hatem.houssein@gmail.com>
2016-02-22 23:47:53 +0100

    whaam

--- src/server/ServerThread.java
+++ src/server/ServerThread.java
@@ -13,6 +13,7 @@ public class ServerThread extends Thread {
 
     private Socket socket;
     private boolean fileType;
+    private DataOutputStream  output;
 
     public ServerThread(Socket clientSocket) {
         socket = clientSocket;
@@ -20,35 +21,28 @@ public class ServerThread extends Thread {
 
     public void run() {
         BufferedReader input;
-        DataOutputStream  output;
-        String htmlContent = "";
-
         try {
             input = new BufferedReader(new InputStreamReader(socket.getInputStream()));         //Receiving from client
-            output = new DataOutputStream(socket.getOutputStream());      //Sending to Client
-
             String read = input.readLine();
-
             String filePath = httpGetFilePath(read);
-
-            htmlContent = htmlReader("images\\index.html");
-
-           // String htmlFileContents = htmlFile(filePath);
-           // FileInputStream fileName = new FileInputStream(filePath);
-            output.writeBytes(htmlContent);           //Write the html file to the browser
-            System.out.println(htmlContent);
-            output.writeBytes("");
-
-            output.close();
+            htmlReader(filePath);
             input.close();
         } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private String httpGetFilePath(String read) {
-        String tmp = read.substring(read.lastIndexOf(":") + 1);
-        //  System.out.println(tmp);
+    /**
+     * A method to read the URL from the client "browser". Also specifies the type of the required file.
+     * @param read
+     * @return The file path
+     */
+    private String httpGetFilePath(String read) throws Exception{
+
+        String tmp = read.substring(read.lastIndexOf(":") + 1, read.lastIndexOf(" "));
+        System.out.println(tmp);
         String filePath = null;
 
         for(int i = 0; i < tmp.length(); i++){
@@ -60,30 +54,32 @@ public class ServerThread extends Thread {
                 break;
             }
         }
-        //  System.out.println(filePath);
-        if(filePath.endsWith(".html") || filePath.endsWith(".htm"))
-            fileType = false;
-        else if (filePath.endsWith(".png"))
-            fileType = true;
+        filePath = filePath.replace('/', File.separator.charAt(0));
+        System.out.println(filePath);
+        if(!filePath.endsWith("html") && !filePath.endsWith("htm") && !filePath.endsWith("png"))
+            throw new Exception("File type is not allowed.");
+
+//        if(filePath.endsWith(".html") || filePath.endsWith(".htm"))
+//            fileType = false;
+//        else if (filePath.endsWith(".png"))
+//            fileType = true;
         return filePath;
     }
 
-    private String htmlReader (String filePath){
 
+    private void htmlReader (String filePath){
         try {
-        File myHTML = new File (filePath);
-        Scanner reader = new Scanner(myHTML); // create scanner to 'read' text
-        String returnThis = "";
-
-        while (reader.hasNextLine()){
-            returnThis = returnThis + reader.nextLine() + "\n"; //attach all lines of text to String
-        }
+            File myHTML = new File (filePath);
+            FileInputStream reader = new FileInputStream(myHTML); // create scanner to 'read' text
+            output = new DataOutputStream(socket.getOutputStream());
+            byte[] buf = new byte[1024];
+            int count;
+            while ((count = reader.read(buf)) >= 0)
+                output.write(buf, 0, count);
+            output.close();
             reader.close();
-            return returnThis;
-
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return "ERROR MESSAGE";
     }
 }
