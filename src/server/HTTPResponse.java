17e184961a90cc8f3049b5c6411d803a05fea59d
yogurt1989 <mauropappaterra@gmail.com>
2016-02-25 17:12:23 +0100

    Merge branch 'master' of C:\Users\zenbook\Dropbox\My Files\Linnaeus University\4. Fourth Semester\Computer Networks - an introduction\Assignment 2\assign2 with conflicts.

--- src/server/HTTPResponse.java
+++ src/server/HTTPResponse.java
@@ -3,59 +3,49 @@ package server;
 import java.io.File;
 
 /**
- * A class to create the HTTP response to the browser
  * Created by Hatem on 2/23/2016.
  */
+
 public class HTTPResponse {
     String response;        //The whole response header
 
     public HTTPResponse(){
-        response = "HTTP-Version: HTTP/1.1 ";
+        response = "HTTP-Version: HTTP/1.0 ";
     }
 
     public void formHTTPResponse(int returnCode, File file, char fileType) {
         switch (returnCode) {
             case 200:
-                response += "200 OK" + "\r\n";
-                getLastModified(file);
+                response += "200 OK\n";
                 getContentLength(file);
                 getContentType(fileType);
                 break;
             case 403:
-                response += "403 Forbidden" + "\r\n";
+                response += "403 Forbidden\n";
                 break;
             case 404:
-                response += "404 File Not" + "\r\n";
+                response += "404 File Not Found\n";
                 break;
             case 500:
-                response += "500 Internal Server Error" + "\r\n";
+                response += "500 Internal Server Error\n";
                 break;
         }
-        getServerId();
-    }
-
-    private void getLastModified(File file) {
-        response += "Last-Modified: "+ file.lastModified() + "\r\n";
     }
 
     private void getContentLength(File file){
-        response += "Content-Length: " + file.length() + "\r\n";
+        response += "Content-Length: " + file.length() + "\n";
     }
 
     private void getContentType(char fileType){
         if(fileType =='h')
-            response += "Content-Type: text/html" + "\r\n";
+            response += "Content-Type: text/html\n";
         else if(fileType == 'p')
-            response += "Content-Type: image/png" + "\r\n";
+            response += "Content-Type: image/png\n";
         else if (fileType == 'i')
-            response += "Content-Type: image/x-icon" + "\r\n";
-    }
-
-    private void getServerId(){
-        response += "Server: Hatem & Mauro simple server" + "\r\n";
+            response += "Content-Type: image/x-icon\n";
     }
 
     public String getResponse() {
-        return response + "\r\n";
+        return response;
     }
 }
